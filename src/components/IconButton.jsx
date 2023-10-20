4fa84de2437af845c9a674d6f18dfa3750d38535
Up Crown <wangshangguan@hotmail.com>
2023-10-20 01:26:36 -0500

    add featured NFT

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IconButton = ({icon, text}) => {
   return (
-    <button className='flex items-center gap-2 bg-purple-500 text-white my-5'>
+    <button className='flex items-center gap-2 bg-purple-500 text-white my-7 py-4 px-6 rounded-xl'>
         {icon}{text}
     </button>
   )
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import {IconButton, NumberCard} from '../components';
 import { BsFillRocketTakeoffFill } from 'react-icons/bs';
+import AutumnMxse from '../assets/AutumnMxse.png';
+import GxngYxng from '../assets/gxngyxng.jpeg';
 
 const Hero = () => {
   return (
-    <section>
-      <article className='w-1/2'>
-        <h1 className='font-bold'>Discover Digital Art & Collect NFTs</h1>
-        <p className='capitalize text-xl'>Collect, buy and sell art from more than 20k NFT artists.</p>
+    <section className='lg:flex gap-12'>
+      <article className='lg:w-1/2'>
+        <h1 className='font-bold my-7'>Discover Digital Art & Collect NFTs</h1>
+        <p className='capitalize text-xl'>Collect, buy and sell art from more than 370k NFT artists.</p>
         <IconButton icon={<BsFillRocketTakeoffFill />} text="Get Started" />
-        <div className='flex justify-between'>
+        <div className='flex justify-between mt-10'>
           <NumberCard number="240" text="Total Sale" />
           <NumberCard number="100" text="Auctions" />
           <NumberCard number="370" text="Artists" />
         </div>
+      </article>
+      <article className='w-96 rounded-2xl text-left bg-gray-300'>
+          <img className='w-96 rounded-2xl' src={AutumnMxse} alt="AutumnMxse is an NFT artwork created by artist GxngYxng" />
+            <h3 className='font-bold text-xl ml-3 mt-3'>Autumn Mxse</h3>
+            <div className='flex gap-2 ml-3 mb-3'>
+              <img className='w-5 rounded-full' src={GxngYxng} alt="Artist GxngYxng's headshot"/>
+              <span>GxngYxng</span>
+            </div>
       </article>
     </section>
   )
