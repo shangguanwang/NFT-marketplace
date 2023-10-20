92389002a74fa5e4196a8637256c67bda03bd848
Up Crown <wangshangguan@hotmail.com>
2023-10-20 12:51:12 -0500

    create singleGetStarted card and getStartedData

--- src/views/GetStarted.jsx
+++ src/views/GetStarted.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { SingleGetStarted } from '../components';
+import { getStartedData } from '../assets/getStartedData';
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <section className='mt-10 m-5'>
+      <h2 className='text-left text-3xl font-bold my-5'>How It Works</h2>
+      <p className='text-left capitalize text-xl'>Find out how to get started</p>
+      <div className='flex'>
+        {getStartedData.map((item)=> <SingleGetStarted key={item.id} image={item.image} title={item.title} description={item.description}/>)}
+      </div>
+    </section>
   )
 }
 
