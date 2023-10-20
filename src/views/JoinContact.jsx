7e0a0f8e6b65c926758159d2a0570805551e4714
Up Crown <wangshangguan@hotmail.com>
2023-10-20 13:39:01 -0500

    Create JoinContact component

--- src/views/JoinContact.jsx
+++ src/views/JoinContact.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import newspaper from '../assets/newspaper.jpg';
+import { IconButton } from '../components';
+import {AiOutlineMail} from 'react-icons/ai';
 
 const JoinContact = () => {
   return (
-    <div>JoinContact</div>
+    <section className='lg:flex gap-16 mt-10 lg:m-8 px-5 lg:px-20 py-10 bg-gray-500 rounded-2xl'>
+      <img className='rounded-2xl w-96' src={newspaper} alt="Reading newspaper" />
+      <article className='lg:w-1/2 text-left text-white'>
+      <h2 className='text-xl lg:text-3xl font-bold my-5 capitalize'>Join our weekly digest</h2>
+      <p className='capitalize text-lg lg:text-xl'>Get exclusive promotions & updates straight to your inbox.</p>
+      <div className='flex items-center h-14 mt-6'>
+        <input type='text' name="email" id="email" placeholder='Enter Your Email Here' className='lg:w-2/3 p-4 rounded-l-xl'></input>
+        <div className='transform -translate-x-5'>
+          <IconButton icon={<AiOutlineMail />} text="Subscribe"/>
+        </div>
+      </div>
+      </article>
+    </section>
   )
 }
 
