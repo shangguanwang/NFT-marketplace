bb30ab6588c46184918c7a0847d76bb28512e508
Up Crown <wangshangguan@hotmail.com>
2023-10-20 11:54:33 -0500

    modify margin for singleCategory

--- src/components/SingleCategory.jsx
+++ src/components/SingleCategory.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SingleCategory = ({bgimg, icon, text}) => {
   return (
-    <article className='relative w-64 bg-gray-500 rounded-2xl'>
+    <article className='relative w-64 bg-gray-500 rounded-2xl m-3'>
         <img className='w-64 rounded-2xl blur-sm' src={bgimg} alt={`${text} category`} />
         <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-12 text-white'>
             {icon}
--- src/views/Category.jsx
+++ src/views/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
   return (
     <section className='mt-10 m-5'>
       <h2 className='text-left text-3xl font-bold my-5'>Browse Categories</h2>
-      <div className='lg:grid lg:grid-rows-2 lg:grid-cols-4 gap-4'>
+      <div className='lg:grid lg:grid-rows-2 lg:grid-cols-4'>
         {categoryData.map((category)=> <SingleCategory key={category.id} bgimg={category.imgUrl} icon={category.icon} text={category.title} />)}
       </div>
     </section>
