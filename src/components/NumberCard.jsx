b24433504fa75f4df22e57dda52a075da65a81e6
Up Crown <wangshangguan@hotmail.com>
2023-10-21 10:48:42 -0500

    modify heading level for NumberCard

--- src/components/NumberCard.jsx
+++ src/components/NumberCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NumberCard = ({number, text}) => {
   return (
     <div className='m-2'>
-        <h3 className='font-bold text-xl'>{number}k+</h3>
+        <h2 className='font-bold text-xl'>{number}k+</h2>
         <p className='text-xl'>{text}</p>
     </div>
   )
